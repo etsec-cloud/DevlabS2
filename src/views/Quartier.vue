b0ef879a0acb8e45c3f7407b4b3c34f0114d902f
etsec-cloud <etienne.secondini@hotmail.fr>
2020-04-27 02:22:01 +0200

    setup encore

--- src/views/Quartier.vue
+++ src/views/Quartier.vue
@@ -1,10 +1,12 @@
 <template>
 
-<div class="container">
-
-
+<div class="wrapper">
+  <div class="flexContainer">
+    <h1>Shinjuku</h1>
+    <img class="kanji" src="./../assets/shinju.svg">
+    <span class="lol"></span>
+  </div>
 </div>
-
 </template>
 
 
@@ -21,8 +23,36 @@ export default {
 
 
 <style scoped>
- .test{
-  background:  url("./../assets/shibuyaBg.jpg");
  
- }
+.wrapper {
+  height:100vh;
+  background:linear-gradient(rgba(0, 0, 0, 0.45),rgba(0, 0, 0, 0.45)),url("./../assets/bg.jpg" ) ;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;}
+
+.flexContainer{
+  display: flex;
+  flex-direction: column;
+
+  justify-content: space-around;
+  align-items: center;
+
+  color:white;
+  font-size: 8em;
+  line-height: 100px;
+}
+.kanji{
+  width: 50vw;
+}
+
+.lol {
+width: 0;
+height: 0;
+border-style: solid;
+border-width: 103.9px 60px 0 60px;
+border-color: #ffffff transparent transparent transparent;
+
+}
+
 </style>
