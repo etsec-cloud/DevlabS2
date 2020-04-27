d1283ba6bdce679394fbee56839ac8cd78c4600a
etsec-cloud <etienne.secondini@hotmail.fr>
2020-04-27 17:10:14 +0200

    icon triangle

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+import NavBar from './components/NavBar.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+
+  render: h => h(App),
+}).$mount('#app')
+
+
+import VueMapbox from "vue-mapbox";
+import Mapbox from "mapbox-gl";
+
+Vue.use(VueMapbox, { mapboxgl: Mapbox });
