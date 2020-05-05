34609c27b8dc3ce84f4b2ded5c861da3b43020b0
etsec-cloud <etienne.secondini@hotmail.fr>
2020-05-05 11:39:18 +0200

    debug

--- src/views/Asakusa.vue
+++ src/views/Asakusa.vue
@@ -2,20 +2,24 @@
 <div>
     <FirstScreenAsakusa/>
     <SecondScreenAsakusa/>
+    <ThridScreenAsakusa/>
 </div>
 </template>
 
 
 <script>
 
-import FirstScreenAsakusa from './../components/FirstScreenAsakusa.vue'
-import SecondScreenAsakusa from './../components/SecondScreenAsakusa.vue'
+import FirstScreenAsakusa from './../components/assakusa/FirstScreenAsakusa'
+import SecondScreenAsakusa from './../components/assakusa/SecondScreenAsakusa.vue'
+import ThridScreenAsakusa from './../components/assakusa/ThirdScreenAsakusa.vue'
+
 
 export default {
   name : 'Asakusa',
   components:{
     FirstScreenAsakusa,
-    SecondScreenAsakusa
+    SecondScreenAsakusa,
+    ThridScreenAsakusa
     },
   
 };
