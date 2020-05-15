c8c54eac440ab8733d6d3235d4fa474b855752db
elsighon <alexandre.duval6@gmail.com>
2020-05-15 12:59:22 +0200

    carteMap

--- src/components/asakusa/SecondScreenAsakusa.vue
+++ src/components/asakusa/SecondScreenAsakusa.vue
@@ -1,37 +1,40 @@
 <template>
 
 <div class="container">
-  <div class="colonne1">
-    <div id="menu" >
-      <div  
+  <ul id="list" class="colonne1">
+    <div id="menu">
+    
+      <li
         @mouseover="hover = true"
         @mouseleave="hover = false"
         :class="{ active: hover }"
-       >
-         
+       >   
+       
+        <img class="imageMap" src="./../../assets/asakusaMap.jpg"/> 
+      </li>
 
-        <img class="imageMap" src="./../../assets/asakusaMap.jpg" /> 
-      </div>
-      <div class="trait"></div>
-      <div class="text1" v-scroll-to="'#text2bis'">Culture/Evènement </div> 
-      <div class="trait"></div>
-      <div class="text1" v-scroll-to="'#text3'">Nourriture</div>
-      <div class="trait"></div>
-      <div class="text1" v-scroll-to="'#text4bis'">Lieux incontournables</div>
-      <div class="trait"></div>
-      <div class="text1" v-scroll-to="'#text5'">Spécialité</div>
-      <div class="trait"></div>
-  </div>
+      <li        class="trait"></li>
+      <li id="1" class="text1" v-scroll-to="'#text2bis'">Culture/Evènement </li> 
+      <li        class="trait"></li>
+      <li id="2" class="text1" v-scroll-to="'#text3'">Nourriture</li>
+      <li        class="trait"></li>
+      <li id="3" class="text1" v-scroll-to="'#text4bis'">Lieux incontournables</li>
+      <li        class="trait"></li>
+      <li id="4" class="text1" v-scroll-to="'#text5'">Spécialité</li>
+      <li        class="trait"></li>
+    </div>
+  </ul>
+  
 
-  </div>
-
-  <div id="lol" class="colonne2">
-    <h1  >Culture/Evènement</h1>
+<span id="lol"></span>
+  <div  class="colonne2">
+    <h1>Culture/Evènement</h1>
     <div class="image_left"></div>
     <div id="text2">Lorem  ici alll ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
   </div>
 
-  <div class="colonne3">
+  <div  class="colonne3">
+   
     <div id="text2bis">Lorem ipsum dolor sit amet,  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
     <div class="image_right"></div>
   </div>
@@ -52,78 +55,77 @@ export default {
   created () {
     window.addEventListener('scroll', this.stickymenu);
     window.addEventListener('scroll', this.bold);
-
-
   },
   destroyed () {
     window.removeEventListener('scroll', this.stickymenu);
     window.removeEventListener('scroll', this.bold);
-
-
   },
-
   methods: {
-
    bold: function(){
-
+     //element dans le container
       var colonne1 = document.getElementById("lol");
-      var coul = document.querySelector('.text1');
-      var bold = colonne1.offsetHeight;
-      
-      if (window.pageYOffset >= bold) {
-        coul.classList.add("text1maj");
+      var colonne2 = document.getElementById("dota");
+      var colonne3 = document.getElementById("cs");
+      var colonne4 = document.getElementById("vcs");
+      //element dans le menu
+      var id1 = document.getElementById('1');
+      var id2 = document.getElementById('2');
+      var id3 = document.getElementById('3');
+      var id4 = document.getElementById('4');
+      if (window.pageYOffset > (colonne1.offsetTop))  { 
+          id1.classList.add("text1maj");
       }
-
-      else if (window.pageYOffset < bold){
-        coul.classList.remove("text1maj");
+      if (window.pageYOffset > (colonne2.offsetTop)) {
+          id1.classList.remove("text1maj");
+          id2.classList.add("text1maj");
       }
-
+      if (window.pageYOffset > (colonne3.offsetTop )){
+        id2.classList.remove("text1maj");
+        id3.classList.add("text1maj");
+      }
+      if (window.pageYOffset > (colonne4.offsetTop )){
+        id3.classList.remove("text1maj");
+        id4.classList.add("text1maj");
+      }
+      if (window.pageYOffset < (colonne4.offsetTop )){
+        id4.classList.remove("text1maj");
+      }
+      if (window.pageYOffset < (colonne3.offsetTop )){
+        id3.classList.remove("text1maj");
+      }
+      if (window.pageYOffset < (colonne2.offsetTop )){
+        id2.classList.remove("text1maj");
+      }
+      if (window.pageYOffset < (colonne1.offsetTop )){
+        id1.classList.remove("text1maj");
+      }
+    
     },
-
     stickymenu: function(){
-
       var colonne1 = document.getElementById("menu");
-      var coul = document.getElementById("lol");
       var sticky = colonne1.offsetHeight;
-
       var lo = 250;
       
       if (window.pageYOffset - lo > sticky) {
-        console.log(coul.offsetHeight)
-        
         colonne1.classList.add("sticky");
       }
-
       else{
         colonne1.classList.remove("sticky");
       }
-
     },
-
-  Bold: function(){
-
-      var text1 = document.getElementById("text1maj");
-      var text1maj = text1.offsetHeight;
-      var lo = 50;
-      
-      if (window.pageYOffset - lo > text1maj) {
-        text1.classList.add("text1maj");
-      }
-
-      else{
-        text1.classList.remove("text1maj");
-      }
-
-    },
-
   }
 };
-
 </script>
 
 
 <style>
-
+li{
+  list-style: none;
+}
+ul{
+  margin: 0;
+  padding: 0;
+}
 .active {
   position:absolute;
   top: 50%; 
@@ -131,7 +133,7 @@ export default {
   transform: translate(-50%, -50%);
   width: 100vw;
   margin-left: 40vw;
-  margin-top: 5vw;
+  margin-top: 15vh;
 }
 .container{
   height:100vh;
@@ -139,23 +141,20 @@ export default {
 	flex-direction: row;
 	flex-wrap: nowrap;
 	justify-content: space-around;
+  padding-bottom: 30px;
 }
-
 .sticky {
   position: fixed;
   top: 0;
   width: 20%;
   margin-top: 50px;
 }
-
 .colonne1{
   width: 20%;
   margin-top: 50px;
   justify-content: center;
   transition-duration:1s,
-
 }
-
 .trait{
   background-color: black;
   height: 3px;
@@ -164,27 +163,22 @@ export default {
   margin-top: 12%;
   margin-bottom: 12%;
 }
-
 .text1maj{
   text-align: center;
   font-size: 30px;
   font-weight: bold;
   }
-
 .text1{
   text-align: center;
   font-size: 20px;
 }
-
 .imageMap{
   width: 50%;
   
 }
-
 .colonne2{
   width: 37%;
 }
-
 .image_left{
   height: 70%;
   width: 100%;
@@ -193,21 +187,17 @@ export default {
   background-size:contain;
   background-repeat: no-repeat;
 }
-
 .text2{
   text-align: center;
   margin-top: 30px;
 }
-
 .colonne3{
   width: 37%;
   margin-top: 50px;
 }
-
 .text2bis{
   text-align: center;
 }
-
 .image_right{
   height: 70%;
   width: 100%;
@@ -217,17 +207,14 @@ export default {
   margin-top: 50px;
 }
 @media screen and (max-width: 900px) {
-
   .container{
   flex-direction: column ;
   justify-content: center;
   align-items: center;
-
   }
   #menu{
   display: flex;
   flex-direction: row;
   }
 }
-
 </style>
